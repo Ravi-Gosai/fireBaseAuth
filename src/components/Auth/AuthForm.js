bfc3da41fcc32a7f18bd973068edcc976ddc50f2
Ravi Gosai <rbgosai86@gmail>
2024-04-22 08:55:36 +0530

    login auth done

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -4,6 +4,7 @@ import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const inputEmailRef = useRef();
   const inputPasswordRef = useRef();
 
@@ -13,34 +14,50 @@ const AuthForm = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-
+    setIsLoading(true);
     const enteredEmail = inputEmailRef.current.value;
     const enteredPassword = inputPasswordRef.current.value;
-
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCur9xCsh35ycJRAqP2U3DynKEpK8MDbj8";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCur9xCsh35ycJRAqP2U3DynKEpK8MDbj8",{
-          method : 'POST',
-          body : JSON.stringify({
-            email : enteredEmail,
-            password : enteredPassword,
-            returnSecureToken : true 
-          }),
-          headers : {
-            'Content-Type': 'application/json'
-          }
-        }
-      ).then((res)=>{
-        if(res.ok){
-
-        }else{
-          return res.json().then((data)=>{
-            console.log(data)
-          })
-        }
-      })
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCur9xCsh35ycJRAqP2U3DynKEpK8MDbj8";
+         
     }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+
+        return res.json()
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = " authencaton failed";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          // alert(errorMessage);
+          // console.log(data);
+          throw new Error(errorMessage)
+        });
+      }
+    }).then((data)=>{
+      console.log(data)
+    }).catch(err=>{
+      alert(err.message)
+    })
   };
 
   return (
@@ -69,13 +86,16 @@ const AuthForm = () => {
           }}
           className={classes.actions}
         >
-          <button
-            type="submit"
-            className={classes.toggle}
-            onClick={submitHandler}
-          >
-            {isLogin ? "login" : " create account"}
-          </button>
+          {!isLoading && (
+            <button
+              type="submit"
+              className={classes.toggle}
+              onClick={submitHandler}
+            >
+              {isLogin ? "login" : " create account"}
+            </button>
+          )}
+          {isLoading && <p>sending a request</p>}
         </div>
         <div className={classes.actions}>
           <button
